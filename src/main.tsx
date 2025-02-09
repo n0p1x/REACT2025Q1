15e2607d01e84269047daf5ad23e5577459dff50
n0p1x <181847497+n0p1x@users.noreply.github.com>
2025-02-10 00:33:53 +0200

    chore: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
 createRoot(rootElement).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* TODO: Add 404 page */}
+          <Route path="*" element={<h2>Not Found</h2>} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   </StrictMode>
 );
